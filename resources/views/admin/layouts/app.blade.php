0e7d2645db58db075d3534bac6ba31b86633466a
Manson Xasthur <mansonx13@gmail.com>
2019-10-11 21:57:15 +0200

    bug fixes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Digital Sigma') }}</title>
+    <title>{{ config('app.name', 'Elixir') }}</title>
 
 
     <!-- Fonts -->
